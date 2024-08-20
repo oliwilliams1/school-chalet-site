18a30609d9d4a3305c563f4478bb611a0005a5a6
oliwilliams1 <imhungryoliver@gmail.com>
2024-08-20 14:55:35 +1200

    Add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,46 @@
-export default function IndexPage() {
-  return (<h1>Hello World!</h1>);
+import Head from "next/head";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@nextui-org/button";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
+
+export default function Index() {
+  return (
+    <div>
+      <Navbar shouldHideOnScroll isBordered>
+        <NavbarBrand>
+          <Image
+            src="/favicon.ico"
+            alt="logo"
+            width={36}
+            height={36}
+            className="rounded-full"
+          />
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Other page 1
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Other page 2
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      </Navbar>
+    </div>
+  )
 }
