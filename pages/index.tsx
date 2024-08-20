f3802d37ae575f957606b58f90052fa165156778
oliwilliams1 <imhungryoliver@gmail.com>
2024-08-20 21:45:01 +1200

    Create small blockouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ export default function Index() {
   return (
     <div>
       <Header />
+
       <div className="overflow-hidden" ref={emblaRef}>
         <div className="flex">
           <div className="flex-[0_0_100%] min-w-0 h-[80vh] bg-slate-200">Slide 1</div>
@@ -15,6 +16,12 @@ export default function Index() {
           <div className="flex-[0_0_100%] min-w-0 h-[80vh] bg-slate-400">Slide 3</div>
         </div>
       </div>
+
+      <div className="w-full h-48 bg-slate-500">Temporary order form</div>
+      
+      <div className="w-full h-screen bg-slate-400">Something</div>
+
+      <div className="w-full h-96 bg-slate-300">Footer</div>
     </div>
   )
 }
