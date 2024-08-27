2e0f48ac4f9b54fb1f08df11698db142b37b6fec
oliwilliams1 <imhungryoliver@gmail.com>
2024-08-27 23:42:54 +1200

    fix build errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -102,7 +102,7 @@ export default function Index() {
       <div className="w-full bg-slate-900 p-24">
         <h2 className="text-white text-4xl text-center mb-8">Discover the Wonders of Maunga Ski Resort</h2>
         <p className="text-white text-lg text-center max-w-[60%] ml-auto mr-auto">
-          Experience the thrill of skiing and snowboarding on our world-class slopes, surrounded by the breathtaking beauty of the Maunga mountain range. With over 50 runs catering to all skill levels, there's something for everyone to enjoy.
+          Experience the thrill of skiing and snowboarding on our world-class slopes, surrounded by the breathtaking beauty of the Maunga mountain range. With over 50 runs catering to all skill levels, there{"'"}s something for everyone to enjoy.
         </p>
       </div>
 
